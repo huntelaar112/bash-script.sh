763090b5d0f1bb3c633107ecf1b8c0d821f699d4
mannk98 <khacman98@gmail.com>
2024-03-19 17:49:30 +0700

    update

--- app.sh/security/tool/run.sh
+++ app.sh/security/tool/run.sh
@@ -8,6 +8,25 @@ exitval=0
 
 namescrpit=${0:2}
 
+function Wpscan() {
+  URL_SCAN="${1}"
+  echo "website:" "$URL_SCAN"
+  domain=$(echo "$URL_SCAN" | sed 's|.*//\([^/]*\).*|\1|')
+  docker run --rm \
+    --mount type=bind,source="$(pwd)/${resultDir}/wpscan-report",target=/output \
+    wpscanteam/wpscan:latest \
+    -o /output/"$domain".txt \
+    --url "${URL_SCAN}" \
+    --api-token "${WPS_TOKEN}" \
+    -e vp --random-user-agent \
+    --plugins-detection mixed
+  #    --http-auth "${HTTP_AUTH}"
+
+  [[ $? ]] && {
+    log-info "Resutl save to $(pwd)/${resultDir}/$domain.txt"
+  }
+}
+
 function help() {
   echo "Usage: ${namescrpit} config.env -dwsc
   Note: This script need sudo permission to execute.
@@ -88,19 +107,12 @@ option="${2}"
   #  docker run --rm --mount type=bind,source=$(pwd)/${resultDir}/wpscan-report,target=/output wpscanteam/wpscan:latest \
   #    -o /output/wpscan-output.txt --url ${URL_SCAN} --api-token {WPS_TOKEN} -e vp --plugins-detection mixed --http-auth 'drill:drill@2021$'
 
-  echo "website:" $URL_SCAN
-  docker run --rm \
-    --mount type=bind,source="$(pwd)/${resultDir}/wpscan-report",target=/output \
-    wpscanteam/wpscan:latest \
-    -o /output/wpscan-output.txt  \
-    --url "${URL_SCAN}" \
-    --api-token "${WPS_TOKEN}" \
-    -e vp --random-user-agent \
-    --plugins-detection mixed
-#    --http-auth "${HTTP_AUTH}"
+  IFS=', ' read -r -a URL_ARRAY <<<"$URL_SCANs"
 
-  [[ $? ]] && {
-    log-info "Resutl save to $(pwd)/${resultDir}/wpscan-output.txt"
-  }
+  # Print the elements of the array
+  for url in "${URL_ARRAY[@]}"; do
+    Wpscan "$url" &
+  done
+  wait
 }
 
